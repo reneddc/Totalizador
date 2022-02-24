b6f19768a1306cbaa473f93b2424aee34c501c90
“Rene <rene.diaz@ucb.edu.bo>
2022-02-23 21:59:55 -0400

    Commit 1 Punto_1

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,9 @@
-import sumar from "./sumador";
-
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
-const div = document.querySelector("#resultado-div");
+const cantidadItems = document.querySelector("#cantidad-items");
+const form = document.querySelector("#totalizar-form");
+const cantidadItemsDiv = document.querySelector("#cantidad-items-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
-
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  cantidadItemsDiv.innerHTML = "<p>" + cantidadItems.value + "</p>";
 });
