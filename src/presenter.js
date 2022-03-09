35a51276ccf75a91379b931cdb8f1167f10e0e53
“Rene <rene.diaz@ucb.edu.bo>
2022-03-09 18:55:29 -0400

    Funcion 1

--- src/presenter.js
+++ src/presenter.js
@@ -1,9 +1,10 @@
 const cantidadItems = document.querySelector("#cantidad-items");
 const form = document.querySelector("#totalizar-form");
 const cantidadItemsDiv = document.querySelector("#cantidad-items-div");
-
+ 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-
-  cantidadItemsDiv.innerHTML = "<p>" + cantidadItems.value + "</p>";
+ 
+  cantidadItemsDiv.innerHTML = "<p>Cantidad de Items: " + cantidadItems.value + "</p>";
 });
+
