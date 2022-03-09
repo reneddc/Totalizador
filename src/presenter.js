f9fb6635fe811b2a416a51aef4f0d9e051afd85f
“Rene <rene.diaz@ucb.edu.bo>
2022-03-09 18:59:38 -0400

    Funcion 3

--- src/presenter.js
+++ src/presenter.js
@@ -6,12 +6,16 @@ const valoresDiv = document.querySelector("#valores-div");
 //Valores input
 const cantidadItems = document.querySelector("#cantidad-items");
 const precioItems = document.querySelector("#precio-items");
+const codigoEstado = document.querySelector("#codigo-estado");
+ 
  
 form.addEventListener("submit", (event) => {
   event.preventDefault();
  
   valoresDiv.innerHTML = `<p>Cantidad de Items: ${cantidadItems.value} </p>
-                          <p>Precio por Item: $ ${precioItems.value} </p>`;
+                          <p>Precio por Item: $ ${precioItems.value} </p>
+                          <p>Codigo de Estado: ${codigoEstado.value} </p>`;
 });
+
 
 
